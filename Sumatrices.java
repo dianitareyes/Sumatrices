26ba26146d2574f7cbd08fd7b3d2135125b95a82
Minorusal <minorusal@hotmail.com>
2024-12-10 15:43:22 -0600

    Se crea un objeto scanner para permitir la entrada de datos y se utliza para leer los valaores, en las variables se aplican las matrices bidimensionales las cuales son A y B, S almacenará y calculara la suma de las matrices

---
+++ Sumatrices.java
@@ -0,0 +1,9 @@
+public class Sumatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] A = new int[3][3];
+        int[][] B = new int[3][3];
+        int[][] S = new int[3][3];
+    }
+
+}        

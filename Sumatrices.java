9d505199c7108d2f0b30b6ac48c4d43e7fc70abc
Minorusal <minorusal@hotmail.com>
2024-12-10 17:32:10 -0600

    permite al usuario llenar filas por fila y columna por columna los valores de la matriz de 3x3, asegurando de que los datos ingresados sean válidos

--- Sumatrices.java
+++ Sumatrices.java
@@ -4,6 +4,19 @@ public class Sumatrices {
         int[][] A = new int[3][3];
         int[][] B = new int[3][3];
         int[][] S = new int[3][3];
+
+         // Matriz A
+        System.out.println("Ingrese los elementos de la matriz A (3x3):");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("A[" + i + "][" + j + "]: ");
+                while (!scanner.hasNextInt()) { 
+                    System.out.print("Entrada no válida. Intente nuevamente: ");
+                    scanner.next();
+                }
+                A[i][j] = scanner.nextInt();
+            }
+        }
     }
 
 }        
